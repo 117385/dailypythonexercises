8eb84f7afb67ac4814df0512becafe4c5847c219
Armando Crews <33334542+117385@users.noreply.github.com>
2018-02-16 13:47:14 -0700

    Add files via upload

---
+++ armandocrewspythonDE1.py
@@ -0,0 +1,9 @@
+#Armando Crews - Python Daily 1 - 02162018
+#instructions - Write a statement that concatenates at least two variables (you can have more than 2 for this practice).
+# use the string and integer functions, no arithmetic will be allowed for this exercise (i.e. - x*y), 
+# combine two variables for a string a variable. The key word here is CONCATENATE.
+
+x = 'Hello'
+y = 'World!'
+
+print(x+y)
